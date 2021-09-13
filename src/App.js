918f307ffe0c4acfdbefede5699b876b7555a74d
Swayam Gupta <swayamgupta5705367@gmail.com>
2021-09-13 18:24:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
+import axios from "axios"
 
 function App() {
+  const [url, setUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState(null)
+
+
+  const onInputChange = (e) => {
+    setUrl(e.target.value)
+  }
+
+  const submitForm = async (e) => {
+    e.preventDefault()
+    const shortUrl = (await axios.post("http://localhost:5000/shortenUrl", {originalUrl: url})).data.shortenUrl
+    console.log(shortUrl)
+    setShortUrl(shortUrl)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>URL Shortener</h1>
+        <form className="form-group" onSubmit={submitForm}>
+          <label>URL</label>
+          <input type="text" placeholder="Url" className="form-control" value={url} onChange={onInputChange}></input>
+          <br/>
+          <button type="submit" className="btn btn-secondary">Submit </button>
+        </form>
+
+        <br></br>
+
+        {shortUrl && <a href={shortUrl} target="_blank" rel="noreferrer">{shortUrl}</a>}
+      </div>
     </div>
   );
 }
